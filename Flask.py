acdf51e20d6b283a8d7ee65c8b662c703c827fb2
Lucas Gómez Torres <lucasgomeztorres@MacBook-Pro-de-Lucas.local>
2023-12-13 21:58:28 +0100

    otro commit

--- Flask.py
+++ Flask.py
@@ -43,5 +43,5 @@ def realizar_inferencia():
 
 if __name__ == '__main__':
    #app.run(port=8010)
-    port = int(os.environ.get("PORT", 5000))
-    app.run(port=port)
+    #port = int(os.environ.get("PORT", 5000))
+    app.run()
